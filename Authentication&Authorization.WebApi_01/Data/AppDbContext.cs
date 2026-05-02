866b79d153cb978924c4f75c1c6ee7813004be73
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-05-02 16:34:36 +0630

    Add project files.

---
+++ Authentication&Authorization.WebApi_01/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication_Authorization.WebApi_01.Data
+{
+    public class AppDbContext :IdentityDbContext
+    {
+        public AppDbContext()
+        {
+            
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+           
+        }
+    }
+}
